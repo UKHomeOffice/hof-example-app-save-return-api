4044f136b85415f768deae03febe595d5e14a957
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2022-02-15 14:07:40 +0000

    add create form

---
+++ server/models/forms.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const config = require('../../config');
+const knexfile = require(`${config.migrationsRepo}`);
+const knexfileConfig = knexfile[process.env.NODE_ENV ? 'production' : 'development'];
+const tableName = config.tableName;
+const knex = require('knex')(knexfileConfig);
+
+const timeout = 1000;
+const selectableProps = [
+  'id',
+  'session',
+  'created_at',
+  'updated_at'
+];
+
+const create = props => {
+  if (props.id) {
+    return knex(tableName).where({
+      id: props.id
+    })
+      .update({session: props.session, updated_at: knex.fn.now()})
+      .returning(selectableProps)
+      .timeout(timeout);
+  }
+
+  return knex.insert(props)
+    .returning(selectableProps)
+    .into(tableName)
+    .timeout(timeout);
+};
+
+module.exports = {
+  create
+};
